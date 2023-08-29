07968bc029b2db88cbba8e59b952e9f6598f5254
Ronak Paul <ronakpaul882@gmail.com>
2023-08-29 13:03:27 +0530

    Created the Feed component
    Signed-off-by: Ronak Paul <ronakpaul882@gmail.com>

---
+++ src/Feed.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+
+const Feed = () => {
+    return (
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" placeholder="Start a post"/>
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption Icon={ImageIcon} title="Image" color="#70B5f9" />
+                    <InputOption Icon={SubscriptionIcon} title="Video" color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Feed;
