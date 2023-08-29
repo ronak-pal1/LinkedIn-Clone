1a6754cbd50d08fc4d2a00a2df1907f225ad43dd
Ronak Paul <ronakpaul882@gmail.com>
2023-08-29 11:13:52 +0530

    fixes some typos in classname
    Signed-off-by: Ronak Paul <ronakpaul882@gmail.com>

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -7,7 +7,7 @@ const HeaderOption = ({avatar ,Icon, title}) => {
         <div className="headerOption">
             { Icon && <Icon className="headerOption__icon" />}
             { avatar && <Avatar className="headerOption__icon" src={avatar} />}
-            <h3 className="headerOption_title">{ title }</h3>
+            <h3 className="headerOption__title">{ title }</h3>
         </div>
     );
 }
