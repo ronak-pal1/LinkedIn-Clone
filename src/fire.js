946320291fb3a23cb02f4ccb090c0c3a968dbe39
Ronak Paul <98758085+ronak-pal1@users.noreply.github.com>
2023-10-08 11:02:19 +0530

    Update fire.js
    hiding firebase keys

--- src/fire.js
+++ src/fire.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtmwbqjG8xqCNSmr9MkcMjL9mL0ONin4Y",
-    authDomain: "linkedin-clone-dd3f0.firebaseapp.com",
-    projectId: "linkedin-clone-dd3f0",
-    storageBucket: "linkedin-clone-dd3f0.appspot.com",
-    messagingSenderId: "615298439965",
-    appId: "1:615298439965:web:7bd648a5f2378694a91ff4"
+   //your configration
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
